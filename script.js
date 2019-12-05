e230f67652e7ae7f567a957f146cff8b2bc3d43f
las5h4 <loganasmalley@gmail.com>
2019-12-04 20:03:16 -0600

    finished the main part of the exercise, all astronauts are listed with their pictures

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+let buildSkillsList = function(index){
+    let skillsList = ""
+    for (let j = 0; j < (index.length - 1); j++){
+        
+        skillsList += index[j]+", "        
+    }
+    skillsList += index[index.length - 1]
+    return skillsList
+}
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response){
         response.json().then( function(json){
             let container = document.getElementById("container")
-            container.innerHTML = `
-                <div class = "astronaut">
-                    <div class = "bio">
-                        <h3>${json[0].firstName} ${json[0].lastName}</h3>
-                        <ul>
-                            <li>Hours In Space: ${json[0].hoursInSpace}</li>
-                            <li>Active: ${json[0].active}</li>
-                        </ul>
+            for (let i = 0; i < json.length; i++) {
+                container.innerHTML += `
+                    <div class = "astronaut">
+                        <div class = "bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours In Space: ${json[i].hoursInSpace}</li>
+                                <li>Active: ${json[i].active}</li>
+                                <li>Skills: ${buildSkillsList(json[i].skills)}
+                            </ul>
+                        </div>
+                        <img class = "avatar" src = ${json[i].picture}></img>
                     </div>
-                </div>
-            `
+                `
+            }
         })
     })
 })
