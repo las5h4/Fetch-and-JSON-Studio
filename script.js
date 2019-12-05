212bc9931678081a5e7e1d1cd266ee86471cca29
las5h4 <loganasmalley@gmail.com>
2019-12-04 20:27:25 -0600

    added first bonus mission - sorts astronauts by hours spent in space

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let buildSkillsList = function(index){
 window.addEventListener("load", function(){
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response){
         response.json().then( function(json){
+            json.sort(function(a, b){return b.hoursInSpace - a.hoursInSpace})
             let container = document.getElementById("container")
             for (let i = 0; i < json.length; i++) {
                 container.innerHTML += `
