ca27d0dc9a0115c3f538e84d900a5a89da55b265
las5h4 <loganasmalley@gmail.com>
2019-12-04 19:36:23 -0600

    got the fetch working and posting info for first astronaut

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response){
+        response.json().then( function(json){
+            let container = document.getElementById("container")
+            container.innerHTML = `
+                <div class = "astronaut">
+                    <div class = "bio">
+                        <h3>${json[0].firstName} ${json[0].lastName}</h3>
+                        <ul>
+                            <li>Hours In Space: ${json[0].hoursInSpace}</li>
+                            <li>Active: ${json[0].active}</li>
+                        </ul>
+                    </div>
+                </div>
+            `
+        })
+    })
+})
